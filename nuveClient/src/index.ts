4b2e85984a250fb6078e3e02a91675368b0a48b7
KRTirtho <krtirtho@gmail.com>
2021-11-03 22:23:13 +0600

    Repalces node-fetch with axios in nuve-client

--- nuveClient/src/index.ts
+++ nuveClient/src/index.ts
@@ -1,6 +1,5 @@
 import crypto, { BinaryLike } from "crypto";
-import path from "path"
-import fetch, { Headers } from "node-fetch"
+import axios, { Method } from "axios"
 
 export interface NuveClientOptions {
   service: string;
@@ -127,64 +126,50 @@ class NuveClient {
     return Buffer.from(hex).toString('base64');
   }
 
-  private async send<T extends Record<any, any> | null | undefined, B = Record<any, any>>(method: string = "GET", url?: string, body?: B, params?: Partial<NuveClientOptions> | null, username?: string, role?: string): Promise<T | void> {
-    let service: string = params?.service ?? this.params.service;
-    let key: BinaryLike = params?.key ?? this.params.key;
-    url = path.join(params?.url ?? this.params.url, url ?? "")
+  private async send<T extends Record<any, any> | null | undefined, B = Record<any, any>>(method: Method = "GET", url?: string, body?: B, params?: Partial<NuveClientOptions> | null, username?: string, role?: string): Promise<T | void> {
+    try {
+      let service: string = params?.service ?? this.params.service;
+      let key: BinaryLike = params?.key ?? this.params.key;
+
+      if (!service || !key) {
+        console.log('ServiceID and Key are required!!');
+        return;
+      }
+
+      const timestamp = new Date().getTime();
+      const cnounce = Math.floor(Math.random() * 99999);
+
+      let toSign = timestamp + ',' + cnounce;
+
+      let header = 'MAuth realm=http://marte3.dit.upm.es,mauth_signature_method=HMAC_SHA1';
+
+      if (username && role) {
+
+        username = this.formatString(username);
+
+        header += `,mauth_username=${username},mauth_role=${role}`;
+
+        toSign += `,${username},${role}`
+      }
+
+      const signed = this.calculateSignature(toSign, key);
 
 
-    if (!service || !key) {
-      console.log('ServiceID and Key are required!!');
-      return;
+      header += `,mauth_serviceid=${service},mauth_cnonce=${cnounce},mauth_timestamp=${timestamp},mauth_signature=${signed}`;
+
+
+      const res = await axios({ baseURL: params?.url ?? this.params.url, url, method, headers: { "Authorization": header, 'Content-Type': 'application/json', }, data: body ?? {} });
+
+      const json: T = res.data;
+
+      if (res.status === 205) return json
+      else if ([100, 200, 201, 202, 203, 204].includes(res.status)) return
+      else {
+        throw new Error(JSON.stringify({ status: res.status, responseBody: json }));
+      }
     }
-
-    const timestamp = new Date().getTime();
-    const cnounce = Math.floor(Math.random() * 99999);
-
-    let toSign = timestamp + ',' + cnounce;
-
-    let header = 'MAuth realm=http://marte3.dit.upm.es,mauth_signature_method=HMAC_SHA1';
-
-    if (username && role) {
-
-      username = this.formatString(username);
-
-      header += ',mauth_username=';
-      header += username;
-      header += ',mauth_role=';
-      header += role;
-
-      toSign += ',' + username + ',' + role;
-    }
-
-    const signed = this.calculateSignature(toSign, key);
-
-
-    header += ',mauth_serviceid=';
-    header += service;
-    header += ',mauth_cnonce=';
-    header += cnounce;
-    header += ',mauth_timestamp=';
-    header += timestamp;
-    header += ',mauth_signature=';
-    header += signed;
-
-    const headers = new Headers()
-
-    headers.set("Authorization", header)
-
-    if (body) {
-      headers.set('Content-Type', 'application/json')
-    }
-
-    const res = await fetch(url, { method, body: body ? JSON.stringify(body) : null, headers })
-
-    const json: T = (await res.json()) as T;
-
-    if (res.status === 205) return json
-    else if ([100, 200, 201, 202, 203, 204].includes(res.status)) return
-    else {
-      throw new Error(JSON.stringify({ status: res.status, responseBody: json }));
+    catch (error: any) {
+      throw error?.response
     }
   }
 }
